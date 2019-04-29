b4cd5ded1914e569c241c875e855585f6e2af994
henryhtm <henryhtm@gmail.com>
2019-04-29 11:45:30 +0800

    Update adduser.sh
    通过参数USERLIST可以配置FTP用户名和密码

--- ftpServer/file/adduser.sh
+++ ftpServer/file/adduser.sh
@@ -1,13 +1,33 @@
-useradd -m ftp-read
-useradd -m ftp-write
-useradd -m ftp-admin
-echo -e "r1122d\nr1122d" | passwd ftp-read
-echo -e "w2244r\nw2244r" | passwd ftp-write
-echo -e "a1313n\na1313n" | passwd ftp-admin
-echo -e "local_root=/mnt/ftp-dir" > /etc/vsftpd/user_config/ftp-admin
-echo -e "write_enable=YES" >> /etc/vsftpd/user_config/ftp-admin
-echo -e "local_root=/mnt/ftp-dir" > /etc/vsftpd/user_config/ftp-read
-echo -e "write_enable=NO" >> /etc/vsftpd/user_config/ftp-read
-echo -e "local_root=/mnt/ftp-dir" > /etc/vsftpd/user_config/ftp-write
-echo -e "write_enable=YES" >> /etc/vsftpd/user_config/ftp-write
-echo -e "cmds_allowed=ABOR,CWD,LIST,MDTM,MKD,NLST,PASS,PASV,PORT,PWD,QUIT,RETR,RNFR,RNTO,SIZE,STOR,TYPE,USER,REST,CDUP,HELP,MODE,NOOP,REIN,STAT,STOU,STRU,SYST,FEAT" >> /etc/vsftpd/user_config/ftp-write
+#!/bin/sh
+if [ "$USERLIST"X = ""X ] ; then
+        export USERLIST="WR,ftp-read,rd1354 ad,ftp-admin,a1313n"
+fi
+for userInfo in $USERLIST
+do
+        uType=$(echo $userInfo|cut -d , -f 1)
+        uName=$(echo $userInfo|cut -d , -f 2)
+        uPwd=$(echo $userInfo|cut -d , -f 3)
+        if [ "$uType"x = ""x ] || [ "$uName"x = ""x ] || [ "$uPwd" = ""x ]; then
+                echo "The form of [$userInfo] is Error."
+        else
+                if [ "$uType"x = "RD"x -o "$uType"x = "rd"x ] ; then
+                        echo "Add Read User:"$uName" Passwd:"$uPwd
+                        echo -e "local_root=/mnt/ftp-dir" > /etc/vsftpd/user_config/$uName
+                        echo -e "write_enable=NO" >> /etc/vsftpd/user_config/$uName
+                elif [ "$uType"x = "AD"x -o "$uType"x = "ad"x ] ; then
+                        echo "Add Admin User:"$uName" Passwd:"$uPwd
+                        echo -e "local_root=/mnt/ftp-dir" > /etc/vsftpd/user_config/$uName
+                        echo -e "write_enable=YES" >> /etc/vsftpd/user_config/$uName
+                elif [ "$uType"x = "WR"x -o "$uType"x = "wr"x ] ; then
+                        echo "Add Write User:"$uName" Passwd:"$uPwd
+                        echo -e "local_root=/mnt/ftp-dir" > /etc/vsftpd/user_config/$uName
+                        echo -e "write_enable=YES" >> /etc/vsftpd/user_config/$uName
+                        echo -e "cmds_allowed=ABOR,CWD,LIST,MDTM,MKD,NLST,PASS,PASV,PORT,PWD,QUIT,RETR,RNFR,RNTO,SIZE,STOR,TYPE,USER,REST,CDUP,HELP,MODE,NOOP,REIN,STAT,STOU,STRU,SYST,FEAT" >> /etc/vsftpd/user_config/$uName
+                else
+                        echo "Invalid UserType:"$uType
+                        continue
+                fi
+                useradd -m $uName
+                echo -e $uPwd"\n"$uPwd | passwd $uName
+        fi
+done
