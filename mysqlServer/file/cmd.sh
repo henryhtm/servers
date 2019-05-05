04eb5f0359d50efb4cc7f816c12780ddc3d473e9
henryhtm <henryhtm@gmail.com>
2019-05-05 09:21:32 +0800

    Update cmd.sh

--- mysqlServer/file/cmd.sh
+++ mysqlServer/file/cmd.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-CONF_FILE="/etc/mysql/mysql.conf.d/mysql.cnf"
+CONF_FILE="/etc/mysql/mysql.conf.d/mysqld.cnf"
 if [ "$EXT_PORT"X != ""X ] ; then
-  sed -i "/^port/i\port\t\t= $EXT_PORT" $CONF_FILE && sed -i "/^port/n; /^port/d" $CONF_FILE 
+  sed -i "/^port/i\port\t\t= $EXT_PORT" $CONF_FILE && sed -i "/^port/n; /^port/d" $CONF_FILE
 fi
 if [ "$DATA_DIR"X != ""X ] ; then
-  sed -i "/^datadir/i\datadir\t\t= $DATA_DIR" $CONF_FILE && sed -i "/^datadir/n; /^datadir/d" $CONF_FILE 
+  sed -i "/^datadir/i\datadir\t\t= $DATA_DIR" $CONF_FILE && sed -i "/^datadir/n; /^datadir/d" $CONF_FILE
+  chmod +222 $DATA_DIR
 fi
 
 /etc/init.d/mysql start &
@@ -12,3 +13,4 @@ while true
 do
   sleep 2
 done
+
